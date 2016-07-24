d69b6a57de24203959b4a11d5b03ee8f7db2222c
Moses Byanyuma <mbyanyuma@student.cscc.edu>
2016-07-24 01:32:12 -0400

    replaced mysqli with PDO, converted mySQL data to json

--- data.php
+++ data.php
@@ -1,29 +1,40 @@
 <?php
-
 #Programmer: Moses Byanyuma
 
-	//require_once('includes/database.php');
-    require('./database.php');
+require('./.env');
 
-	$stmt = mysqli_prepare($con, "SELECT (timestamp * 1000) as time, temp as temperature FROM measurements ORDER BY time ASC");
-    
-	//$result = array('time' -> array(), 'temperature' -> array());
-	if ($stmt) {
-		mysqli_stmt_execute($stmt);
-		mysqli_stmt_bind_result($stmt, $time, $temperature);
-	    while (mysqli_stmt_fetch($stmt)) {
-	       echo '['.$row["time"].',' .$row["temperature"].']';
-            $count++;
-            if ($count<"20"){
-            echo ',';
-            }      
-            }
-            echo ']';
-            }
-            else {
-            echo "[[],[]]";
-	    }
-	    mysqli_stmt_close($stmt);
-    }
- 
-?>
+
+// Opens a connection to a mySQL server
+try {
+	$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME. ';charset=utf8',  DB_USERNAME, DB_PASSWORD);
+} catch (PDOException $e) {
+    echo "An Error occured, could not connect!";
+}
+
+$statement = $db->query('SELECT timestamp FROM 032096ff152541ae');
+$rows = array();
+$rows['name'] = 'Timestamp';
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $rows['data'][] = $row['timestamp'];   
+}
+
+$statement = $db->query('SELECT temp FROM 032096ff152541ae');
+$rows1 = array();
+$rows1['name'] = 'Temp';
+while ($rrow = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $rows1['data'][] = $rrow['temp'];  
+}
+
+$statement = $db->query('SELECT humid FROM 032096ff152541ae');
+$rows2 = array();
+$rows2['name'] = 'Humid';
+while ($rrrow = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $rows2['data'][] = $rrrow['humid'];  
+}
+
+$result = array();
+array_push($result,$rows);
+array_push($result,$rows1);
+array_push($result, $rows2);
+
+print json_encode($result, JSON_NUMERIC_CHECK);
